45f59f26e6864adf2deb4db9c3c139514c37268e
Ayzal-fatima <iqrankhannwl@gmail.com>
2024-02-27 00:36:41 +0500

    custome management command

--- custom_management_command/management_commands/admin.py
+++ custom_management_command/management_commands/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Question,Choice
 # Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["id", "question_text", "pub_date"]
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ["id", "choice_text", "question", "votes"]
